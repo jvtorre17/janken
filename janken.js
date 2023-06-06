5d80b11d383d2951a2bf671ec16f6894e39f7774
Oats <jvtorre17@gmail.com>
2023-06-06 21:40:42 +0800

    Computer chooses Rock, Paper, Scissors at random

--- janken.js
+++ janken.js
@@ -2,7 +2,18 @@
 function getComputerChoice() {
   var ComputerChoice = Math.floor(Math.random() * 3)
   
-  console.log(ComputerChoice)
+    if (ComputerChoice == 0) {
+        console.log("Rock");
+    }
+
+    else if (ComputerChoice == 1){
+        console.log("Paper");
+    }
+
+    else if(ComputerChoice == 2){
+        console.log("Scissors");
+    }
+
 }
 
 getComputerChoice()
